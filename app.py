740b81967ef74e3bf0b2969fdec9f0b910b0b3fc
derek-ye <0derek.ye@gmail.com>
2020-01-18 17:46:15 -0800

    Add API calls to app.py and update html files

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import flask
 import requests
+import json
 
 app = flask.Flask(__name__)
 
@@ -18,11 +19,40 @@ def weatherdata():
 
 
 # API calls
-def getWeatherData(coords: list):  # data comes in form [x-coord, y-coord]
+def getWeatherDataLoc(loc: str): 
+    """
+    loc: "CITY_NAME"
+    """ 
+    #check for 404 errors
+
+    # get rid of key
+    gMapsKey = 'AIzaSyBAQ5Sz2UnnVnfz-57UQSJeE2gesyDdgDE'
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s' %(loc, gMapsKey)
+    response = requests.get(url, verify=True)
+    coords = convertLocToCoords(response)
+    getWeatherDataCoords(coords)
+
+def getWeatherDataCoords(coords: list):
+    """
+    data comes in form [x-coord, y-coord]
+    """
+
     xcoord, ycoord = coords
-    response = requests.get("https://api.darksky.net/forecast/afb397c644ff5187bae543cefb7eedcc/%d,%d" %(xcoord, ycoord))
+    url = "https://api.darksky.net/forecast/afb397c644ff5187bae543cefb7eedcc/%d,%d" %(xcoord, ycoord)
+    response = requests.get(url, verify=True)
+
+
+
+# helper functions
+def convertLocToCoords(response):
+    """
+    returns API response in [xcoord, ycoord] format
+    """
+    return list(json.loads(response.text)['results'][0]['geometry']['location'].values())         
+
 
 
 if __name__ == '__main__':
     app.debug=True
     app.run()
+    # getWeatherDataLoc("Irvine")
