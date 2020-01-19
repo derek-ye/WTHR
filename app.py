242ab37b820b2786a3f2f18744d566c8ce7f4634
derek-ye <0derek.ye@gmail.com>
2020-01-19 00:23:57 -0800

    Add search functionality to weatherdata searchbar

--- app.py
+++ app.py
@@ -15,12 +15,20 @@ def index():
 
 @app.route('/', methods=['POST'])
 def index_post():
-    city = flask.request.form['city-name']######################
+    city = flask.request.form['city-name'].lower()
+    city = city[0].upper() + city[1:]
     dateStr = getDate()
     return flask.render_template('weatherdata.html', city = city, curr_date = dateStr)
 
 @app.route('/weatherdata')
-def weatherdata(city='Irvine'):###########################
+def weatherdata(city='Irvine'):
+    dateStr = getDate()
+    return flask.render_template('weatherdata.html', city = city, curr_date = dateStr)
+
+@app.route('/weatherdata', methods=['POST'])
+def weatherdata_post():
+    city = flask.request.form['city-name'].lower()
+    city = city[0].upper() + city[1:]
     dateStr = getDate()
     return flask.render_template('weatherdata.html', city = city, curr_date = dateStr)
 
