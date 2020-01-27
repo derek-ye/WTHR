9b1c9ec8c24a6f125afe2fdc0c9f29e38cb17107
derek-ye <0derek.ye@gmail.com>
2020-01-26 23:09:27 -0800

    Add more tests

--- test/test/e2e/main.js
+++ test/test/e2e/main.js
@@ -7,12 +7,20 @@ module.exports = {
       .saveScreenshot('./screenshots/title-test.png')
       .end();
   },
-  'Temperature Test': function(browser) {
+  'Page1 Search Test': function(browser) {
     browser
       .url('http://127.0.0.1:5000/')
-      .waitForElementVisible('body')
-      .assert.title('WTHR')
-      .saveScreenshot('./screenshots/temp-test.png')
+      .setValue('input[type=text]', ['Fremont', browser.Keys.ENTER])
+      .assert.containsText('#weekly-report', 'Weekly Report - Fremont, CA, USA')
+      .saveScreenshot('./screenshots/p1-search-test.png')
+      .end();
+  },
+  'Page2 Search Test': function(browser) {
+    browser
+      .url('http://127.0.0.1:5000/weatherdata')
+      .setValue('input[type=text]', ['Wenzhou', browser.Keys.ENTER])
+      .assert.containsText('#weekly-report', 'Wenzhou, Zhejiang, China')
+      .saveScreenshot('./screenshots/p2-search-test.png')
       .end();
   },
 };
